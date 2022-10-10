4ad13d426be2c45192f17e0963f0e247f1b755f7
TomiCornejo <tomas.cornejo.z@mail.pucv.cl>
2022-10-10 17:43:02 -0300

    Comienzo registrarse

--- src/app/screens/registro-screen/registro-screen.component.ts
+++ src/app/screens/registro-screen/registro-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-registro-screen',
@@ -7,13 +8,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroScreenComponent implements OnInit {
 
-  usuario:string;
-  clave1:string;
-  clave2:string;
+  usuario: string = "";
+  clave1: string = "";
+  clave2: string = "";
+  img: string = "";
+  
+  tipo:string = "Minimarket";
+  minimarket:string = "";
+  codigo:string = "";
 
-  constructor() { }
+  bandera:boolean = false;
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
   }
 
+  sesion(){
+    sessionStorage.setItem('usuario',JSON.stringify({"nombre":this.usuario,"icono":this.img,"tipo":this.tipo}));
+    
+  }
+
+  revisar(){
+    if(this.usuario == "" || this.clave1 == "" || this.clave2 == ""){
+      this.bandera = false;
+    }else if(this.tipo == "Vendedor"){
+      if(this.minimarket == "" || this.codigo == ""){
+        this.bandera = false;
+      }else{
+        this.bandera = true;
+      }
+    }else{
+      this.bandera = true;
+    }
+  }
+
+  cambioTipo(val:string){
+    this.tipo = val;
+    this.revisar();
+  }
+
+  // Obtiene la imagen de html
+  captureFile(event: any) {
+    const img = event.target.files[0];
+    this.extractBase64(img).then((image: any) => {
+      this.img = image.base;
+    });
+  }
+
+  // Transforma las imagenes a base64
+  extractBase64 = async ($event: any) => new Promise((resolve, reject) => {
+    try {
+      const unsafeImg = window.URL.createObjectURL($event);
+      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+      const reader = new FileReader();
+      reader.readAsDataURL($event);
+      reader.onload = () => {
+        resolve({
+          base: reader.result
+        });
+      };
+      reader.onerror = error => {
+        resolve({
+          base: null
+        });
+      };
+      return reader.result;
+    } catch (e) {
+      return null;
+    }
+  });
 }
